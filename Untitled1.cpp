580d2f72064cf9b795c4144cebcd5abba6ee7b5c
patchkrz <ekiraz18@gmail.com>
2020-05-27 00:34:33 +0300

    Indentation yapıldı.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -7,21 +7,25 @@
  *
  */
 
+// header files
 
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
 #include<stdlib.h>
 
+
+// function prototypes
+
 void search_block(FILE* );
 void operator_type(const char* );
-void add_assembly(void);
-void sub_assembly(void);
-void mult_assembly(void);
+void sum_assembly(FILE*);
+void sub_assembly(FILE*);
+void mult_assembly(FILE*);
 
-extern char cfile_name[100] = {0};
 
-extern FILE* fp_assembly_output = NULL; 
+
+extern char cfile_name[100] = {0};   //  In order to call .c file name from other functions
 
 int main (){
 	
@@ -29,34 +33,33 @@ int main (){
    scanf("%[^\n]s",cfile_name);        
    
    
-   //opens the user defined file
-   FILE *fp = NULL;
+   FILE *fp = NULL;  //opens the user defined file
+   
    if(fp=fopen(cfile_name,"r")){
-   	   
        search_block(fp);
-      
    }
    
    else
       printf(".c file cannot be opened.");
     
-    
-   
    getch();
+   
    return 0;
 	
 }
 
 /*
+ *
  * Function "search_block" takes file pointer then by using the  
  * the pointer, it detects the code blocks nested inside the function, 
  * which is previously written inside the file.
+ *
  */
 void search_block(FILE* fp){
    
    char line[100] = {0};
    
-   const char end_of_block[1] = {'}'};
+   const char end_of_block[2] = {'}','\n'};
    
    const char* func = NULL;
    
@@ -64,8 +67,8 @@ void search_block(FILE* fp){
    	  
    	  if((strchr((const char*)(&line), '{') == NULL)){
 		 
-   	     func = strtok((char*)(&line), &end_of_block[0]); 
-   	     operator_type(func);
+	    func = strtok((char*)(&line), &end_of_block[0]); 
+   	    operator_type(func);
 		 // "func" is the remaining part of the block after removing curly brackets.
       }
       
@@ -74,42 +77,44 @@ void search_block(FILE* fp){
 }
 
 /* 
+ *
  * "operator_type" takes the part which contains numbers and symbol of operation as an input.
  * By detecting the symbol of operation, function call is done to write assembly equivalent.
  *
  */
 void operator_type(const char* operation){
+   FILE* fp_assembly_output = NULL; 
    
    char* temp = NULL;
    
-   temp = strchr (operation, '-');
+   temp = strchr (operation, '+');
    if (temp!=NULL)
-      add_assembly();
+      sum_assembly(fp_assembly_output);
    
    temp = strchr (operation, '-');
    if (temp!=NULL)
-      sub_assembly();
-   /*
+      sub_assembly(fp_assembly_output);
+   
    temp = strchr (operation, '*');
    if (temp!=NULL)
-      mult_assembly();
-   */
+      mult_assembly(fp_assembly_output);
+   
    
    return ;
 }
 
 
-void add_assembly(void){
+void sum_assembly(FILE* fp_assembly_output){
    	
    const char new_extension[5] = {".asm"};
    
    char token[100] = {0};   
    
-   for(int i=0; cfile_name[i] != '.'; i++){  // used to delete .c extension
+   for(int i=0; cfile_name[i] != '.'; i++){  
       
       token[i] = cfile_name[i];
       
-   }
+   }// used to delete .c extension
    
    char* output_file_name = strcat(token, new_extension); // used to create a file name concateneted with .asm 
    
@@ -119,26 +124,29 @@ void add_assembly(void){
       printf("Assembly file cannot be created.");
       exit(1);
    }
+   
    else{
       printf("Assembly file is being created.");
 	  fprintf(fp_assembly_output,"sum:\n\tSUB.W   #4, R1\n\tMOV.W   R12, 2(R1)\n\tMOV.W   R13, @R1\n\tMOV.W   2(R1), R12\n\tADD.W   @R1, R12\n\tADD.W   #4, R1\n\tRET\n\n\n");
    }
+   
    fclose(fp_assembly_output);
+   
    return ;
 }
 
 
-void sub_assembly(void){
+void sub_assembly(FILE* fp_assembly_output){
 	
    const char new_extension[5] = {".asm"};
    
    char token[100] = {0};
    
-   for(int i=0; cfile_name[i] != '.'; i++){  // used to delete .c extension
+   for(int i=0; cfile_name[i] != '.'; i++){  
       
       token[i] = cfile_name[i];
       
-   }
+   }// used to delete .c extension
    
    char* output_file_name = strcat(token, new_extension); // used to create a file name concateneted with .asm 
    
@@ -148,25 +156,29 @@ void sub_assembly(void){
       printf("Assembly file cannot be created.");
       exit(1);
    }
+   
    else{
       printf("Assembly file is being created.");
       fprintf(fp_assembly_output,"sub:\n\tSUB.W   #4, R1\n\tMOV.W   R12, 2(R1)\n\tMOV.W   R13, @R1\n\tMOV.W   2(R1), R12\n\tSUB.W   @R1, R12\n\tADD.W   #4, R1\n\tRET\n\n\n");
    }
+   
    fclose(fp_assembly_output);
+   
    return ;
 }
 
-void mult_assembly(void){
+
+void mult_assembly(FILE* fp_assembly_output){
    
    const char new_extension[5] = {".asm"};
    
    char token[100] = {0};
    
-   for(int i=0; cfile_name[i] != '.'; i++){  // used to delete .c extension
+   for(int i=0; cfile_name[i] != '.'; i++){  
       
       token[i] = cfile_name[i];
       
-   }
+   } // used to delete .c extension
    
    char* output_file_name = strcat(token, new_extension); // used to create a file name concateneted with .asm 
    
@@ -176,11 +188,13 @@ void mult_assembly(void){
       printf("Assembly file cannot be created.");
       exit(1);
    }
+   
    else{
       printf("Assembly file is being created.");
       fprintf(fp_assembly_output,"mult:\n\tSUB.W   #4, R1\n\tMOV.W   R12, 2(R1)\n\tMOV.W   R13, @R1\n\tMOV.W   @R1, R13\n\tMOV.W   2(R1), R12\n\tCALL    #__mspabi_mpyi\n\tADD.W   #4, R1\n\tRET\n\n\n");
    }
+   
    fclose(fp_assembly_output);
+   
    return ;
 }
-
